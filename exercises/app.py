180f748462f3f9d1bb9b27c02aa6c7adeaf23850
aboud20-meet <aboud20@meet.mit.edu>
2018-08-09 13:06:46 +0300

    finisheddddddddd

--- exercises/app.py
+++ exercises/app.py
@@ -1,5 +1,5 @@
 from databases import *
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,8 +10,17 @@ def home():
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
-@app.route('/add')
+@app.route('/add', methods=['GET','POST'])
 def add_student_route():
-	return render_template('add.html')
+	if request.method=='GET':
+		return render_template('add.html')
+
+	else:
+		StudentName = request.form['student_name']
+		StudentYear = request.form['student_year']
+
+		add_student(StudentName,StudentYear, finished_lab=False)
+
+		return render_template('response.html', sn=StudentName, sy=StudentYear)
 
 app.run(debug=True)
